f7dd2d264fd8665da23b0ee5bbbc869073e69af2
ichlaura <laupao41@gmail.com>
2023-11-28 20:34:52 +0900

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+ // header
+
 import throttle from "https://cdn.skypack.dev/lodash@4/throttle";
 
 function onScroll() {
@@ -11,3 +13,36 @@ function onScroll() {
 const $$header = document.querySelector(".js-header");
 
 window.addEventListener("scroll", throttle(onScroll, 300));
+
+
+ // swiper
+
+  var swiper = new Swiper('.swiper-container', {
+   
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+
+ // carrusel de productos
+
+
+    // Configuración del carrusel
+    $(document).ready(function(){
+        $('.carousel').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 5,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    });
